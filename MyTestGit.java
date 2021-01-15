ff2c870735140c946c561119105d668b2cbb3a7f
huqi <1293821028@qq.com>
2021-01-15 20:52:56 +0800

    你今天看起来真帅 确实，你也很帅

--- MyTestGit.java
+++ MyTestGit.java
@@ -3,6 +3,7 @@ package com.atguigu.Github;
 public class MyTestGit {
     public static void main(String[] args) {
         System.out.println("你今天看起来真帅！");
+        System.out.println("确实，你也很帅");
     }
 }
 //https://github.com/windy-HQ/TestMyself.git
