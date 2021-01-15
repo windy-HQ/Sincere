c7699a7126b2f9218cba907c16becc4716f70e3b
Myself <1293821028@qq.com>
2021-01-15 20:52:00 +0800

    你今天看起来真帅

---
+++ MyTestGit.java
@@ -0,0 +1,8 @@
+package com.atguigu.Github;
+
+public class MyTestGit {
+    public static void main(String[] args) {
+        System.out.println("你今天看起来真帅！");
+    }
+}
+//https://github.com/windy-HQ/TestMyself.git
