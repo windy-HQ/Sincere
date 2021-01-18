75eae32342be2a372dabd03980f25d86ccb67d2c
huqi <1293821028@qq.com>
2021-01-18 21:17:41 +0800

    idea上面能成功 吗？

--- MyTestGit.java
+++ MyTestGit.java
@@ -4,6 +4,7 @@ public class MyTestGit {
     public static void main(String[] args) {
         System.out.println("你今天看起来真帅！");
         System.out.println("确实，你也很帅");
+        System.out.println("这里能不能成功？");
     }
 }
 //https://github.com/windy-HQ/TestMyself.git
