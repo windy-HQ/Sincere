afdb1371367b1a1d0719d8ab96da4c445c537e0d
huqi <1293821028@qq.com>
2021-01-21 11:09:36 +0800

    再来一次

--- MyTestGit.java
+++ MyTestGit.java
@@ -5,6 +5,7 @@ public class MyTestGit {
         System.out.println("你今天看起来真帅！");
         System.out.println("确实，你也很帅");
         System.out.println("这里能不能成功？");
+	System.out.println("再来试一遍");
     }
 }
 //https://github.com/windy-HQ/TestMyself.git
