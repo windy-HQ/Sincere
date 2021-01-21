09364710c0f3e1cd29fee3ec96f2343b64b7d986
huqi <1293821028@qq.com>
2021-01-21 11:19:43 +0800

    每日一夸

--- MyTestGit.java
+++ MyTestGit.java
@@ -4,7 +4,7 @@ public class MyTestGit {
     public static void main(String[] args) {
         System.out.println("你今天看起来真帅！");
         System.out.println("确实，你也很帅");
-        System.out.println("这里能不能成功？");
+        System.out.println("今天的你又变帅了一点");
 	System.out.println("再来试一遍");
     }
 }
